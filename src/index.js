1e70a3bb91f948fc89e1398a2a8f075da9d0f311
Victor Parizio <victorparizio@hotmail.com>
2023-09-27 16:11:30 -0300

    feat: Adicionado Cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const rotas = require('./rotas')
+const cors = require('cors')
 
 const app = express()
 const PORTA = process.env.PORT || 3000
 
+app.use(cors())
 app.use(express.json())
-
 app.use(rotas)
 
 app.listen(PORTA, () => {
