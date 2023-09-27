066792387dd894243dc50eeb54f592786ad77a72
Ayrton Antunes <ayrtonantunes3@gmail.com>
2023-09-27 17:38:54 -0300

    rotas: login do usuário

--- src/controladores/usuario.js
+++ src/controladores/usuario.js
@@ -18,4 +18,38 @@ const cadastrarUsuario = async (req, res) => {
   }
 }
 
-module.exports = { cadastrarUsuario }
+const loginUsuario = async (req, res) => {
+  const { email, senha } = req.body
+
+  try {
+      const usuario = await pool.query('select * from usuarios where email = $1', [email])
+
+      if (!email || !senha) {
+          return res.status(400).json({ mensagem: 'Todos os campos são obrigatórios.'})
+      }
+
+      if (usuario.rowCount < 1) {
+          return res.status(404).json({ mensagem: 'Usuário e/ou senha inválido(s).'})
+      }
+
+      const senhaValida = await bcrypt.compare(senha, usuario.rows[0].senha)
+
+      if (!senhaValida) {
+          return res.status(401).json({ mensagem: 'Usuário e/ou senha inválido(s).'})
+      }
+
+      const token = jwt.sign({ id: usuario.rows[0].id, }, senhaJwt, { expiresIn: '1h' })
+
+      const { senha: _, ...usuarioLogado } = usuario.rows[0]
+
+      return res.status(200).json({ usuario: usuarioLogado, token })
+
+  } catch (error) {
+      return res.status(500).json({ mensagem: 'Erro interno do servidor' })
+  }
+}
+
+module.exports = { 
+  cadastrarUsuario,
+  loginUsuario
+ }
--- src/rotas.js
+++ src/rotas.js
@@ -1,12 +1,13 @@
 const { Router } = require('express')
 const rotas = Router()
 
-const { cadastrarUsuario } = require('./controladores/usuario')
+const { cadastrarUsuario, loginUsuario } = require('./controladores/usuario')
 const {
   validarCorpo,
   verificarEmailInformado,
 } = require('./intermediarios/usuario')
 
 rotas.post('/usuario', validarCorpo, verificarEmailInformado, cadastrarUsuario)
+rotas.post('/login', loginUsuario)
 
 module.exports = rotas
