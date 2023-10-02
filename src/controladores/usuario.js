3f1168467031a6f627f8a9f3c267934ed3b10c55
Victor Parizio <victorparizio@hotmail.com>
2023-10-02 19:28:21 -0300

    correção erro

--- src/controladores/usuario.js
+++ src/controladores/usuario.js
@@ -35,7 +35,7 @@ const loginUsuario = async (req, res) => {
     return res.status(200).json({ usuario, token })
   } catch (error) {
     console.log({ error: error.detail, mensagem: error.message })
-    return res.status(500).json({ mensagem: 'Erro interno do Servidor' })
+    return res.status(500).json({ mensagem: error.message })
   }
 }
 
